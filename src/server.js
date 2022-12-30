618fb614315a4be1c59875b20743d033f8c4a1b8
SeefatHimel <Seefat.hossain@brainstation-23.com>
2022-12-31 02:34:09 +0600

    tst

--- src/server.js
+++ src/server.js
@@ -15,11 +15,14 @@ const keys = require("./data/oauth2.keys.json");
 const app = express();
 
 mongoose.connect(
-  "mongodb://localhost/socialLogin",
-  () => {
-    console.log("mongodb connected");
-  },
-  (e) => console.error(e)
+  "mongodb+srv://himel:himel@cluster0.6uvuj.mongodb.net/test",
+  (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("connected");
+    }
+  }
 );
 
 const corsOptions = {
@@ -123,7 +126,10 @@ app.post("/token", (req, res) => {
   if (refreshTokens[0]) return res.sendStatus(403);
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-    const accessToken = generateAccessToken({ name: user.name });
+    const accessToken = generateAccessToken({
+      user: user?.name,
+      email: user?.email,
+    });
     res.json({ accessToken: accessToken });
   });
 });
@@ -204,9 +210,8 @@ app.get("/getData", authenticateToken, async (req, res) => {
 });
 
 async function registerUser(userReq, res) {
-  console.log("userReq.email", userReq.email);
+  console.log(userReq);
   const emailValid = await check_email(userReq.email);
-  console.log("Email is", emailValid ? "Valid" : "invalid");
   if (emailValid) {
     // Creating empty user object
     const newUser = new User();
@@ -222,32 +227,27 @@ async function registerUser(userReq, res) {
     try {
       newUser.save((err, User) => {
         if (err) {
-          console.log("225", err);
+          console.log(err);
           return res.status(400).send({
             message: "Failed to add user.",
           });
         } else {
-          console.log("User Creation Successful");
           return res.status(201).send({
             message: "User added successfully.",
           });
         }
       });
     } catch (error) {
-      console.log("err 236", error);
+      console.log(error, "err");
       return res.status(400).send({
         message: "Failed to add user.",
       });
     }
-  } else {
-    return res.status(403).send({ message: "Email already in use or Invalid" });
   }
 }
 
 app.post("/signUp", async (req, res) => {
-  console.log("res.body", req.body);
-  // console.log("req", req);
-  await registerUser(req.body, res);
+  await registerUser(req.body.data, res);
   console.log("246", "/signUp", "ok");
   // res.status(200).send({ message: "User Created !!" });
 
@@ -255,12 +255,14 @@ app.post("/signUp", async (req, res) => {
 });
 
 app.post("/signIn", async (req, res) => {
+  console.log("email : ", req.body.email, " Pass : ", req.body.password);
   await User.findOne({ email: req.body.email }, function (err, user) {
-    if (user === null) {
+    if (!user) {
       return res.status(400).send({
         message: "User not found.",
       });
     } else {
+      console.log(user);
       if (user.validPassword(req.body.password)) {
         const accessToken = generateAccessToken({
           user: user?.name,
@@ -302,13 +304,11 @@ async function check_email(email) {
 }
 
 app.post("/register_email", async (req, res) => {
-  console.log("Email > ", req.body, req.body.email);
-  if (req?.body?.email) {
-    const validEmail = await check_email(req.body.email);
-    console.log("Valid Email : ", validEmail);
-    if (validEmail) res.status(200).send({ message: "email not in use" });
-    else res.status(403).send({ message: "email already in use" });
-  } else res.status(403).send({ message: "Invalid email" });
+  console.log("Email > ", req.body.data);
+  const validEmail = await check_email(req.body.data.email);
+  console.log("Valid Email : ", validEmail);
+  if (validEmail) res.status(200).send({ message: "email not in use" });
+  else res.status(403).send({ message: "email already in use" });
   // res.send(validEmail.name || req.body.email);
 });
 
@@ -326,7 +326,6 @@ app.post("/logout", async (req, res) => {
   } else res.status(200).send({ message: "Logged out!!" });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("server running on port", port);
+app.listen(3000, () => {
+  console.log("server running");
 });
