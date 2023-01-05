1f2ba7a877c6eac366950459be820d91abe46a8b
SeefatHimel <Seefat.hossain@brainstation-23.com>
2023-01-06 01:15:28 +0600

    uri render

--- src/server.js
+++ src/server.js
@@ -77,7 +77,7 @@ app.get("/", (req, res) => {
 const oAuth2Client = new OAuth2Client(
   keys.web.client_id,
   keys.web.client_secret,
-  keys.web.redirect_uris[1]
+  keys.web.redirect_uris[0]
 );
 
 async function getGoogleTokens(code, res) {
